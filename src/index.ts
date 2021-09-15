2494a2a4f49f7eff2c9ccb4fa417f792c5286dfa
Henrique de Almeida <oedudealmeida@gmail.com>
2021-09-15 11:54:28 -0300

    Criação de end-points de Status e de CRUD de usuários

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, {Request, Response, NextFunction} from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'bar' });
-});
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+// Configurações de rotas
+app.use(usersRoute);
+app.use(statusRoute);
+
+// Inicialização do servidor
 app.listen(3000, () => {
     console.log('funciona');
 });
